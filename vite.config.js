c2ed0785d2e50df315b405778a0797978b3f8023
ProgramMoh <mohamed.elnaggar@ucalgary.ca>
2025-09-27 20:07:54 -0600

    base url update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Add this 'base' property
-  base: '/HealthyLife/',
+  base: '/bold/',
   plugins: [react()],
 })
